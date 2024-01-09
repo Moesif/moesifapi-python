3dcff1db48ef1abd5d6a786c35fb2089177bc05c
xiluan <xinyujie0320@gmail.com>
2024-01-09 15:00:47 -0800

    fix logger format issue

--- moesifapi/api_logger.py
+++ moesifapi/api_logger.py
@@ -3,17 +3,10 @@ import logging
 
 class APILogger:
     def __init__(self, name,
-                 level=logging.DEBUG,
-                 log_format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'):
-        formatter = logging.Formatter(log_format)
-
-        # handler
-        customize_handler = logging.StreamHandler()
-        customize_handler.setFormatter(formatter)
+                 level=logging.DEBUG):
 
         self.logger = logging.getLogger(name)
         self.logger.setLevel(level)
-        self.logger.addHandler(customize_handler)
 
     def get_logger(self):
         return self.logger
