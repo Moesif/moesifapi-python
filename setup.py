594a2c65afed145cd983500ed8a9f8c532b98fa4
Keyur <keyur.kv@gmail.com>
2024-04-08 16:22:06 -0700

    Refactor: Include dependencies on install_require
    Refactor: Include dependencies on install_require
    Bump version to 1.4.5

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.4.4',
+    version='1.4.5',
 
     description='Moesif API Lib for Python',
     long_description=long_description,
@@ -83,7 +83,10 @@ setup(
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
     install_requires=['requests', 'jsonpickle',
-                      'python-dateutil', 'isodatetimehandler'],
+                      'python-dateutil', 'isodatetimehandler',
+                      'readerwriterlock>=1.0.9',
+                      'apscheduler>=3.10.4'
+                      ],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
